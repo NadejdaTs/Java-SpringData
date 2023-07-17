ae50f4c31ea3fc549d5c8c5aea3aecc7cc8c82e1
NadejdaTs <nadezhda.tsvetanova@gmail.com>
2023-07-17 19:01:05 +0300

    Demo from Lab

---
+++ Spring_Data_Lab/src/main/java/com/example/spring_data_lab/SpringDataLabApplication.java
@@ -0,0 +1,15 @@
+package com.example.spring_data_lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataLabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDataLabApplication.class, args);
+    }
+
+    
+
+}
